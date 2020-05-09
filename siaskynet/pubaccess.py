7c872436ae90aa98d2b032aa9274ea1b5dd5ce03
EvilRedHorse <39168275+EvilRedHorse@users.noreply.github.com>
2020-05-09 12:03:19 -0400

    Update and rename skynet.py to pubaccess.py

--- siaskynet/pubaccess.py
+++ siaskynet/pubaccess.py
@@ -5,17 +5,17 @@ import string
 import requests
 
 
-class Skynet:
+class Pubaccess:
 
     @staticmethod
-    def uri_skynet_prefix():
-        return "sia://"
+    def uri_pubaccess_prefix():
+        return "scp://"
 
     @staticmethod
     def default_upload_options():
         return type('obj', (object,), {
-            'portal_url': 'https://siasky.net',
-            'portal_upload_path': 'skynet/skyfile',
+            'portal_url': 'https://scp.techandsupply.ca',
+            'portal_upload_path': 'pubaccess/pubfile',
             'portal_file_fieldname': 'file',
             'portal_directory_file_fieldname': 'files[]',
             'custom_filename': ''
@@ -24,23 +24,23 @@ class Skynet:
     @staticmethod
     def default_download_options():
         return type('obj', (object,), {
-            'portal_url': 'https://siasky.net',
+            'portal_url': 'https://scprime.hashpool.eu',
         })
 
     @staticmethod
     def strip_prefix(str):
-        if str.startswith(Skynet.uri_skynet_prefix()):
-            return str[len(Skynet.uri_skynet_prefix()):]
+        if str.startswith(Pubaccess.uri_pubaccess_prefix()):
+            return str[len(Pubaceess.uri_pubaccess_prefix()):]
         return str
 
     @staticmethod
     def upload_file(path, opts=None):
-        return Skynet.uri_skynet_prefix() + Skynet.upload_file_request(path, opts).json()["skylink"]
+        return Pubaccess.uri_pubaccess_prefix() + Pubaccess.upload_file_request(path, opts).json()["publink"]
 
     @staticmethod
     def upload_file_request(path, opts=None):
         if opts is None:
-            opts = Skynet.default_upload_options()
+            opts = Pubaccess.default_upload_options()
 
         with open(path, 'rb') as f:
             host = opts.portal_url
@@ -52,7 +52,7 @@ class Skynet:
     @staticmethod
     def upload_file_request_with_chunks(path, opts=None):
         if opts is None:
-            opts = Skynet.default_upload_options()
+            opts = Pubaccess.default_upload_options()
 
         filename = opts.custom_filename if opts.custom_filename else path
 
@@ -62,8 +62,8 @@ class Skynet:
 
     @staticmethod
     def upload_directory(path, opts=None):
-        r = Skynet.upload_directory_request(path, opts)
-        sia_url = Skynet.uri_skynet_prefix() + r.json()["skylink"]
+        r = Pubaccess.upload_directory_request(path, opts)
+        sia_url = Pubaccess.uri_pubaccess_prefix() + r.json()["publink"]
         r.close()
         return sia_url
 
@@ -74,10 +74,10 @@ class Skynet:
             return
 
         if opts is None:
-            opts = Skynet.default_upload_options()
+            opts = Pubaccess.default_upload_options()
 
         ftuples = []
-        files = list(Skynet.walk_directory(path).keys())
+        files = list(Pubaccess.walk_directory(path).keys())
         for file in files:
             ftuples.append((opts.portal_directory_file_fieldname,
                             (file, open(file, 'rb'))))
@@ -91,35 +91,35 @@ class Skynet:
         return r
 
     @staticmethod
-    def download_file(path, skylink, opts=None):
-        r = Skynet.download_file_request(skylink, opts)
+    def download_file(path, publink, opts=None):
+        r = pubaccess.download_file_request(publink, opts)
         open(path, 'wb').write(r.content)
         r.close()
 
     @staticmethod
-    def download_file_request(skylink, opts=None, stream=False):
+    def download_file_request(publink, opts=None, stream=False):
         if opts is None:
-            opts = Skynet.default_download_options()
+            opts = Pubaccess.default_download_options()
 
         portal = opts.portal_url
-        skylink = Skynet.strip_prefix(skylink)
-        url = f'{portal}/{skylink}'
+        publink = Pubaccess.strip_prefix(publink)
+        url = f'{portal}/{publink}'
         r = requests.get(url, allow_redirects=True, stream=stream)
         return r
 
     @staticmethod
-    def metadata(skylink, opts=None):
-        r = Skynet.metadata_request(skylink, opts)
+    def metadata(publink, opts=None):
+        r = Pubaccess.metadata_request(publink, opts)
         return json.loads(r.headers["skynet-file-metadata"])
 
     @staticmethod
-    def metadata_request(skylink, opts=None, stream=False):
+    def metadata_request(publink, opts=None, stream=False):
         if opts is None:
-            opts = Skynet.default_download_options()
+            opts = Pubaccess.default_download_options()
 
         portal = opts.portal_url
-        skylink = Skynet.strip_prefix(skylink)
-        url = f'{portal}/{skylink}'
+        publink = Pubaccess.strip_prefix(publink)
+        url = f'{portal}/{publink}'
         r = requests.head(url, allow_redirects=True, stream=stream)
         return r
 
@@ -128,8 +128,9 @@ class Skynet:
         files = {}
         for root, subdirs, subfiles in os.walk(path):
             for subdir in subdirs:
-                files.update(Skynet.walk_directory(os.path.join(root, subdir)))
+                files.update(Pubaccess.walk_directory(os.path.join(root, subdir)))
             for subfile in subfiles:
                 fullpath = os.path.join(root, subfile)
                 files[fullpath] = True
         return files
+    
