251758b03fcc9f86b72c79ca1cc3e556b3458306
EvilRedHorse <39168275+EvilRedHorse@users.noreply.github.com>
2020-06-16 11:52:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Pubaccess",
+    name="pubaccess",
     version="1.0.2",
     author="Peter-Jan Brone",
     author_email="peterjan.brone@gmail.com",
